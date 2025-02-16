c10255f2ee9f976402cc72f14b8310c3f93ca909
Olivia Chen <chenhuiya2005@gmail.com>
2025-02-15 23:17:31 -0800

    Initial Commit

---
+++ rpg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rpg
+//
+//  Created by Olivia Chen on 2/15/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
